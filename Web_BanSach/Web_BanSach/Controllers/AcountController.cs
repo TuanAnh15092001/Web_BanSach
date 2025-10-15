d9fc58601cd364bcc94115f2e0ef808be733720c
TuanAnh15092001 <nguyentuan1592001@gmail.com>
2025-10-15 21:04:02 +0700

    thêm tính năng

---
+++ Web_BanSach/Web_BanSach/Controllers/AcountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_BanSach.Controllers
+{
+    public class AcountController : Controller
+    {
+        // GET: Acount
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
