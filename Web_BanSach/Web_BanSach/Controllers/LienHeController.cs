55b15e706458e33d3fc1dcbcb40a82f4a1302dc5
TuanAnh15092001 <nguyentuan1592001@gmail.com>
2025-10-22 13:32:39 +0700

    cap nhap lien he va Banner

---
+++ Web_BanSach/Web_BanSach/Controllers/LienHeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_BanSach.Controllers
+{
+    public class LienHeController : Controller
+    {
+        // GET: LienHe
+        public ActionResult Index()
+        {
+            ViewBag.TieuDe = "Thông tin liên Hệ của nhóm 5";
+            ViewBag.Message = "Web bán sách trực tuyến được thực hiện bởi nhóm 5.";
+            List<string> list = new List<string>();
+            list.Add("1. Nguyễn Tuấn Anh- 20120001 - 0123456789");
+            list.Add("2. La thuận Phát- 20120002 - 0987654321");
+            list.Add("3. Nguyễn Phúc - 20120003 - 0912345678");
+            ViewBag.Nhom5 = list;
+            return View();
+        }
+    }
+}
